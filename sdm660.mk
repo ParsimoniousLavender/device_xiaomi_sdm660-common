ecc2b647339d4351e0ed1719d93b7564f8b4e4f6
Stylogey <stylogey@gmail.com>
2019-08-16 08:35:51 +0200

    sdm660-common: XiaomiParts: Import XiaomiParts
    Conflicts:
    	sdm660.mk

--- sdm660.mk
+++ sdm660.mk
@@ -488,5 +488,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
