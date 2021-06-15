e1aa3c8e04252685476ede4ac7453bed7c5182cb
Martin Štrobl <recordyletsplays@gmail.com>
2021-06-15 15:19:51 +0200

    sdm660-common: Bringup for EvolutionX

--- sdm660.mk
+++ sdm660.mk
@@ -388,7 +388,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay \
-    $(COMMON_PATH)/overlay-lineage
+    $(COMMON_PATH)/overlay-evolution
 
 # RRO configuration
 TARGET_USES_RRO := true
