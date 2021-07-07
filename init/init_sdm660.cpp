f77351f7ad430eb3d55f2d1df3ca63dfbe4b46fc
Omar Hamad <etahamad@icloud.com>
2021-07-07 20:36:36 +0200

    sdm660-common: Update fingerprint to `Redfin - RQ3A.210705.001`

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -119,7 +119,7 @@ void vendor_load_persist_properties()
 void vendor_load_properties()
 {
   property_override("ro.build.description", "lavender-user 10 QKQ1.190910.002 V11.0.1.0.QFGMIXM release-keys");
-  property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys");
+  property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
   
    std::string product = GetProperty("ro.product.vendor.device", "");	
    if (product.find("whyred") != std::string::npos)
