371b3a10b07b3503aa1e41c3f286987ae53e1b05
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    sdm660-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -544,6 +544,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/xiaomi
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # System
 PRODUCT_VENDOR_PROPERTIES += \
     persist.sys.binary_xml=false
