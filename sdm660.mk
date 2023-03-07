d11c8f97f12a70dedb5b4b8f5b74c96ab8c847a6
pix106 <sbordenave@gmail.com>
2023-03-07 18:19:24 +0100

    FIX sdm660-common: Switch audio HAL to V7
    * drop ":32"

--- sdm660.mk
+++ sdm660.mk
@@ -77,8 +77,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.1-impl:32 \
-    android.hardware.audio.effect@7.0-impl:32 \
+    android.hardware.audio@7.1-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl \
     audio.primary.sdm660 \
