1582130940/android_device_xiaomi_sdm660-common

sdm660-common: configs: Add qmi_fw.conf
This file will silence the warnings on qmi

Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>
Change-Id: I7d7ec0da00a33a0da8f189b0181d40919b9918e5

sdm660.mk
@@ -414,6 +414,9 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qmi_fw.conf:$(TARGET_COPY_OUT_VENDOR)/etc/qmi_fw.conf
+
 # RIL
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \

ParsimoniousLavender/device_xiaomi_sdm660-common

Signed-off-by: pix106 <sbordenave@gmail.com>

sdm660.mk
@@ -501,6 +501,9 @@ $(call inherit-product, external/json-c/Android.configure.mk)
 PRODUCT_PACKAGES += \
     libjson
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/qmi_fw.conf:$(TARGET_COPY_OUT_VENDOR)/etc/qmi_fw.conf
+
 # QTI Performance
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \