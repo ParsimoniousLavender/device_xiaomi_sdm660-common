4d5e09c90b95f058feb698a540c51b4b1fcfe01c
pix106 <sbordenave@gmail.com>
2023-08-03 12:39:45 +0200

    sdm660-common: Fix releasetools + adapt existing use of AddImage

--- releasetools.py
+++ releasetools.py
@@ -20,7 +20,7 @@ import re
 
 def FullOTA_InstallBegin(info):
   input_zip = info.input_zip
-  AddImage(info, "RADIO", input_zip, "super_dummy.img", "/tmp/super_dummy.img");
+  AddImage(info, "RADIO", "super_dummy.img", "/tmp/super_dummy.img");
   info.script.AppendExtra('package_extract_file("install/bin/flash_super_dummy.sh", "/tmp/flash_super_dummy.sh");')
   info.script.AppendExtra('set_metadata("/tmp/flash_super_dummy.sh", "uid", 0, "gid", 0, "mode", 0755);')
   info.script.AppendExtra('run_program("/tmp/flash_super_dummy.sh");')
@@ -36,17 +36,18 @@ def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info, input_zip)
   return
 
-def AddImage(info, input_zip, basename, dest):
-  name = basename
-  path = "IMAGES/" + name
+def AddImage(info, dir, basename, dest):
+  input_zip = info.input_zip
+  path = dir + "/" + basename
   if path not in input_zip.namelist():
     return
 
   data = input_zip.read(path)
   common.ZipWriteStr(info.output_zip, basename, data)
+  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
 def OTA_InstallEnd(info, input_zip):
   info.script.Print("Patching device-tree and verity images...")
-  AddImage(info, input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImage(info, "IMAGES", "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   return
