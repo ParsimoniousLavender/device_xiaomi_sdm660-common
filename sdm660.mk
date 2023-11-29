44626d367c73502982ef1ce11b243525434ec531
pix106 <sbordenave@gmail.com>
2023-11-29 21:24:53 +0100

    Revert "sdm660-common: Ship older libhidlbase.so for fingerprint blobs"
    This reverts commit 4ea2d831af3f85911055937cc837540552d6a5c8.

--- sdm660.mk
+++ sdm660.mk
@@ -602,9 +602,6 @@ PRODUCT_PACKAGES += \
     libstdc++_vendor \
     com.android.vndk.current.on_vendor
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
-
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
